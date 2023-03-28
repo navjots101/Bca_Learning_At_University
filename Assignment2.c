2521112dbba2ab61050d4a674d2fe867712e85f7
navjots101 <127727968+navjots101@users.noreply.github.com>
2023-03-28 09:40:58 +0530

    Update Assignment2.c

--- Assignment2.c
+++ Assignment2.c
@@ -1,7 +1,21 @@
 //A number is said to be valid if it is divisible by 8. Write a program that allows the user to keep entering numbers as long as the input is valid and also displays a count of the valid numbers. Use a while loop to complete the task
 
-int main() 
-{
-  
-  return 0;
+#include <stdio.h>
+
+int main() {
+    int input, count = 0;
+    
+    printf("Enter a number: ");
+    scanf("%d", &input);
+    
+    while (input % 8 == 0) {
+        count++;
+        printf("Valid number! Enter another number: ");
+        scanf("%d", &input);
+    }
+    
+    printf("You entered %d valid numbers\n", count);
+    
+    return 0;
 }
+
